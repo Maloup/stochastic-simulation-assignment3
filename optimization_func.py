91192b06b786bacfd49ccdc6f1be2394bdc59b38
Steven Oud <soud@pm.me>
2021-12-11 13:08:13 +0100

    Cooling schedule stuff and T_0 initialization

--- optimization_func.py
+++ optimization_func.py
@@ -5,16 +5,16 @@ from helper import int_cost_lotka_volterra
 
 
 def simulated_annealing(s0, t, P,
-    cost=int_cost_lotka_volterra, cooling_schedule="linear",
-    T_start=100, T_steps=500, sigma=0.1
+    cost=int_cost_lotka_volterra, cooling_schedule="geometrical",
+    T_start=100, T_steps=500, sigma=0.1, alpha=0.9
 ):
-    if cooling_schedule == "linear":
-        #T_sched = np.linspace(T_start, 0.1, T_steps)
-        #T_sched =  [(T_start/(1 + 20*np.log(1 + k))) for k in range(T_steps)]
-        T_sched =  [(T_start/(1 + k)) for k in range(T_steps)]
-        #T_sched = 1/np.linspace(0.001, T_start, T_steps)**1.1
+    if cooling_schedule == "geometrical":
+        T_sched =  [T_start*alpha**k for k in range(T_steps)]
+
     state = s0
     state_cost = cost(state, P, t)
+
+    n_accepted = 0
 
     for T in T_sched:
         state_new = np.zeros(len(state))
@@ -25,19 +25,23 @@ def simulated_annealing(s0, t, P,
                     state_new[i] = new_param
                     break
 
-        #sstate_new = np.random.normal(state, scale=sigma)
-        U = np.random.rand()
-
         new_state_cost = cost(state_new, P, t)
+        if new_state_cost < state_cost:
+            state = state_new
+            state_cost = new_state_cost
+            n_accepted += 1
+            continue
+
         threshold = np.min([
             np.exp(-(new_state_cost - state_cost)/T),
             1
         ])
 
-        print(threshold)
+        U = np.random.rand()
 
         if U <= threshold:
             state = state_new
             state_cost = new_state_cost
+            n_accepted += 1
 
-    return state
+    return state, n_accepted/len(T_sched)
