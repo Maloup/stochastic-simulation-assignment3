f576041545b324de94b2fd2ccf7fd40256ad1de1
Steven Oud <soud@pm.me>
2021-12-14 19:36:55 +0100

    Fix acceptance rate counting

--- optimization_func.py
+++ optimization_func.py
@@ -5,30 +5,30 @@ from helper import int_cost_lotka_volterra, mse, mae, r2
 
 
 def simulated_annealing(s0, t, P,
-    cost=mse, cooling_schedule="geometrical",
-    T_start=100, T_steps=500, gamma=0.1, alpha=0.95, write_costs = False
-):
+        cost=mse, cooling_schedule="geometrical",
+        T_start=100, T_steps=500, eta=0.1, alpha=0.95, write_costs = False
+        ):
     if cooling_schedule == "geometrical":
         T_sched =  [T_start*alpha**k for k in range(T_steps)]
     if cooling_schedule == "linear":
         T_sched = np.linspace(T_start, 1/T_start, T_steps)
     if cooling_schedule == "quadratic":
         T_sched = [T_start/(1 + alpha*k**2) for k in range(T_steps)]
-    
+
     state = s0
     state_cost = int_cost_lotka_volterra(state, P, t, cost)
     costs = []
 
-
+    n = 0
     n_accepted = 0
 
     for T in T_sched:
         costs.append(state_cost)
-        
+
         state_new = np.zeros(len(state))
         for i in range(len(state)):
             while True:
-                new_param = np.random.normal(state[i], scale=gamma*state[i])
+                new_param = np.random.normal(state[i], scale=eta*state[i])
                 if new_param >= 0:
                     state_new[i] = new_param
                     break
@@ -37,13 +37,13 @@ def simulated_annealing(s0, t, P,
         if new_state_cost < state_cost:
             state = state_new
             state_cost = new_state_cost
-            n_accepted += 1
             continue
 
+        n += 1
         threshold = np.min([
             np.exp(-(new_state_cost - state_cost)/T),
             1
-        ])
+            ])
 
         U = np.random.rand()
 
@@ -51,10 +51,10 @@ def simulated_annealing(s0, t, P,
             state = state_new
             state_cost = new_state_cost
             n_accepted += 1
-    
-    costs.append(state_cost)    
-    
+
+    costs.append(state_cost)
+
     if write_costs == True:
-        return state, n_accepted/len(T_sched), state_cost, costs
-    else: 
-        return state, n_accepted/len(T_sched), state_cost
+        return state, n_accepted/n, state_cost, costs
+    else:
+        return state, n_accepted/n, state_cost
